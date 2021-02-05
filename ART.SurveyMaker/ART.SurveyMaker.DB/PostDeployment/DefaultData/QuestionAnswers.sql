40320bddc4086d191f5feeace763a53273900e84
Adam Tamminga <500158560@fvtc.edu>
2021-02-04 19:11:02 -0600

    Fixed default data

--- ART.SurveyMaker/ART.SurveyMaker.DB/PostDeployment/DefaultData/QuestionAnswers.sql
+++ ART.SurveyMaker/ART.SurveyMaker.DB/PostDeployment/DefaultData/QuestionAnswers.sql
@@ -1,45 +1,45 @@
 ﻿BEGIN 
 
 	DECLARE @QuestionId uniqueidentifier;
-	SELECT @QuestionId = Id from tblQuestion where Description = 'What color is the sky?' 
+	SELECT @QuestionId = Id from tblQuestion where Question = 'What color is the sky?' 
 
 	DECLARE @AnswerId uniqueidentifier;
-	SELECT @AnswerId = Id from tblAnswer where Description = 'Blue'
+	SELECT @AnswerId = Id from tblAnswer where Answer = 'Blue'
 
-	INSERT INTO DBO.tbltblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
+	INSERT INTO DBO.tblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
 	VALUES
-	(NEWID(), @QuestionId, @AnswerId, true)
+	(NEWID(), @QuestionId, @AnswerId, 1)
 	
-	SELECT @QuestionId = Id from tblQuestion where Description = 'What color is grass?' 
+	SELECT @QuestionId = Id from tblQuestion where Question = 'What color is grass?' 
 
-	SELECT @AnswerId = Id from tblAnswer where Description = 'Green'
+	SELECT @AnswerId = Id from tblAnswer where Answer = 'Green'
 
-	INSERT INTO DBO.tbltblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
+	INSERT INTO DBO.tblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
 	VALUES
-	(NEWID(), @QuestionId, @AnswerId, true)
+	(NEWID(), @QuestionId, @AnswerId, 1)
 
-	SELECT @QuestionId = Id from tblQuestion where Description = 'What noise does a cow make?' 
+	SELECT @QuestionId = Id from tblQuestion where Question = 'What noise does a cow make?' 
 
-	SELECT @AnswerId = Id from tblAnswer where Description = 'Mooo'
+	SELECT @AnswerId = Id from tblAnswer where Answer = 'Mooo'
 
-	INSERT INTO DBO.tbltblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
+	INSERT INTO DBO.tblQuestionAnswer(Id, QuestionId, AnswerId, IsCorrect)
 	VALUES
-	(NEWID(), @QuestionId, @AnswerId, true)
+	(NEWID(), @QuestionId, @AnswerId, 1)
 
-	SELECT @QuestionId = Id from tblQuestion where Description = 'What noise does a pig make?' 
+	SELECT @QuestionId = Id from tblQuestion where Question = 'What noise does a pig make?' 
 
-	SELECT @AnswerId = Id from tblAnswer where Description = 'Oink'
+	SELECT @AnswerId = Id from tblAnswer where Answer = 'Oink'
 
-	INSERT INTO DBO.tbltblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
+	INSERT INTO DBO.tblQuestionAnswer(Id, QuestionId, AnswerId, IsCorrect)
 	VALUES
-	(NEWID(), @QuestionId, @AnswerId, true)
+	(NEWID(), @QuestionId, @AnswerId, 1)
 
-	SELECT @QuestionId = Id from tblQuestion where Description = 'What noise does a dog make?' 
+	SELECT @QuestionId = Id from tblQuestion where Question = 'What noise does a dog make?' 
 
-	SELECT @AnswerId = Id from tblAnswer where Description = 'Woof'
+	SELECT @AnswerId = Id from tblAnswer where Answer = 'Woof'
 
-	INSERT INTO DBO.tbltblQuestionAnswer (Id, QuestionId, AnswerId, IsCorrect)
+	INSERT INTO DBO.tblQuestionAnswer(Id, QuestionId, AnswerId, IsCorrect)
 	VALUES
-	(NEWID(), @QuestionId, @AnswerId, true)
+	(NEWID(), @QuestionId, @AnswerId, 1)
 
 END
