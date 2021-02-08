44438ee554f7f6d7d3cf0b2c13d3b1826526d1c6
Adam Tamminga <500158560@fvtc.edu>
2021-02-08 17:23:52 -0600

    Added PL Tests Shells

--- ART.SurveyMaker/ART.SurveyMaker.PL.Test/utAnswer.cs
+++ ART.SurveyMaker/ART.SurveyMaker.PL.Test/utAnswer.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ART.SurveyMaker.PL.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class utAnswer
     {
         [TestMethod]
         public void TestMethod1()
---
+++ ART.SurveyMaker/ART.SurveyMaker.PL.Test/utQuestionAnswer.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ART.SurveyMaker.PL.Test
+{
+    [TestClass]
+    public class utQuestionAnswer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
