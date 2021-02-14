b60a28083c9b36c76426f2ac6f5a7f1e3d1f235e
Adam Tamminga <500158560@fvtc.edu>
2021-02-13 18:12:11 -0600

    Added UI rough Windows

---
+++ ART.SurveyMaker/ART.SurveyMaker.UI/QuestionAnswerList.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ART.SurveyMaker.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class QuestionAnswerList : Window
+    {
+        public QuestionAnswerList()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnNewQuestionAnswer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEditQuestionAnswer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnQuestion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
